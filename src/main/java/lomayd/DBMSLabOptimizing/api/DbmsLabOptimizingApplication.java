2d38fa97be85fd8a8790466381af5bd41e9e5d28
lomayd <jinho10167@gmail.com>
2023-06-19 01:15:36 +0000

    feat: Project Initialize

---
+++ src/main/java/lomayd/DBMSLabOptimizing/api/DbmsLabOptimizingApplication.java
@@ -0,0 +1,13 @@
+package lomayd.DBMSLabOptimizing.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmsLabOptimizingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbmsLabOptimizingApplication.class, args);
+	}
+
+}
